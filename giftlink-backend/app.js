b9f1adcdd6107274f4b82d909f87eef83a286792
Aashish Gurung <watashiaashishgurung@gmail.com>
2025-03-04 11:20:46 +0100

    fixing typos indicated from github actions linting

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -43,7 +43,7 @@ app.use((err, req, res, next) => {
 
 app.get("/",(req,res)=>{
     res.send("Inside the server")
-})
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
